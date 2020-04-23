93a79da33e6f73e76f6535fb96cbcd9eb3ff3b46
fayez <fayezmohammed23@gmail.com>
2020-04-23 21:59:45 +0530

    DServers

---
+++ bin/model/parameters.dart
@@ -0,0 +1,6 @@
+class Parameters {
+  final String parameters;
+  final Function onRequest;
+
+  Parameters({this.parameters, this.onRequest});
+}
---
+++ test/DServers_test.dart
@@ -0,0 +1,8 @@
+import 'package:DServers/DServers.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
